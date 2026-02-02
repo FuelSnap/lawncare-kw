df57e50af5522766db56bcd88e46380897eeac82
FuelSnap <kamilniewulis@gmail.com>
2026-02-02 09:40:13 -0500

    Update project

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,4 @@
-// Existing components (default exports)
+// Base UI components
 export { default as Button } from './Button';
 export { default as Input } from './Input';
 export { default as Select } from './Select';
@@ -9,18 +9,3 @@ export { default as Alert } from './Alert';
 export { default as Progress } from './Progress';
 export { default as Accordion } from './Accordion';
 export { default as StepIndicator } from './StepIndicator';
-
-// New components (named exports)
-export { AvailabilityBanner } from './AvailabilityBanner';
-export { PricePreview } from './PricePreview';
-export { BeforeAfterGallery } from './BeforeAfterGallery';
-export { RegionSelector } from './RegionSelector';
-export { WindowCardPicker } from './WindowCardPicker';
-export { SkipTokensExplainer } from './SkipTokensExplainer';
-export { ServiceChecklist } from './ServiceChecklist';
-export { TestimonialCard, TestimonialsSection } from './TestimonialCard';
-export { TrustBadges, LocalProof } from './TrustBadges';
-export { RainPolicyCard } from './RainPolicyCard';
-export { PetFriendlyModule } from './PetFriendlyModule';
-export { FirstCutBanner } from './FirstCutBanner';
-export { StripeTrustBadge, MultiPropertyTeaser } from './StripeTrustBadge';
